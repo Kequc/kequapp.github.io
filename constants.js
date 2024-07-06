38f831f7968bf97a26b023267e396bbd8c082f24
Kequc <v2xyvxkr.1646bpmi@getinbox.io>
2024-07-06 06:25:38 +0200

    Add syntax highlighting

--- constants.js
+++ constants.js
@@ -1,2 +1,6 @@
 export const VERSION = '0.7.0';
 export const INDEX_URL = '/docs/introduction.html';
+export const ASSETS_FOLDER = './assets';
+export const CONTENT_FOLDER = './content';
+export const TEMPLATES_FOLDER = './templates';
+export const DIST_FOLDER = './dist';
