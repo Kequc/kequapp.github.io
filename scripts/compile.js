c4b5ea931ccfbfd7300d612d692a00a68bfb38d5
Kequc <v2xyvxkr.1646bpmi@getinbox.io>
2024-07-07 03:53:57 +0200

    Support cname

--- scripts/compile.js
+++ scripts/compile.js
@@ -8,6 +8,7 @@ import { ASSETS_FOLDER, DIST_FOLDER } from '../constants.js';
 async function main() {
     await fsx.emptyDir(DIST_FOLDER);
     await fsx.copy(ASSETS_FOLDER, path.join(DIST_FOLDER, ASSETS_FOLDER));
+    await fsx.copyFile('./CNAME', path.join(DIST_FOLDER, 'CNAME'));
 
     const content = await getContent();
     await compilePages(content);
